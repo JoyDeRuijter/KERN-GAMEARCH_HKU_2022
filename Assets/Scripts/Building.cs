8ba6b016497692c09ca1c1ac876e3bb1a11fa158
Joy <joyderuijter@gmail.com>
2022-10-07 11:57:13 +0200

    Create the base of the whole inputsystem with the commandpattern, start with the builder/buildingmanager logic

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building
+{
+    public int level;
+    public int price;
+    public GameObject prefab;
+    // IAttackBehavior
+
+    public void Attack()
+    { 
+    
+    }
+
+    public void GetDestroyed()
+    { 
+    
+    }
+}
---
+++ Assets/Scripts/Input/BuildCommand.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildCommand : ICommand
+{
+    private Builder builder;
+
+    public BuildCommand(Builder _builder){ builder = _builder;}
+
+    public void Execute()
+    {
+        // Execute build action
+        builder.BuildBuilding();
+    }
+
+}
